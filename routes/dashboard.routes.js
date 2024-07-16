510134f7b1ebeb15d89baf26b842067a7bd92609
Arief <mrarief0110@gmail.com>
2024-07-16 17:29:36 +0530

    Fix: The DELETE and PUT code for targetting the id_key

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // GET: Get user details
 router.get("/", authMiddleware, getDashboard);
+
 // POST: Create a new user
 router.post("/", async (req, res) => {
   try {
@@ -18,13 +19,54 @@ router.post("/", async (req, res) => {
       socialHandler,
     });
     await newUser.save();
-    res.status(201).json({ message: "User created successfully", user: newUser });
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
-// DELETE: Deleting the user
 
+// PUT: Edit an individual user
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { key ,firstName, lastName, email, phoneNumber, socialHandler } = req.body;
+    const updatedUser = await userList.findByIdAndUpdate(
+      id,
+      {
+        key,
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        socialHandler,
+      },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "User updated successfully", user: updatedUser });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
+// DELETE: Delete an individual user
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await userList.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 module.exports = router;
