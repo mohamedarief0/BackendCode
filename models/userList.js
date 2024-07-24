8e01d2cedb96099605155b4b0012dd415b115de7
Arief <mrarief0110@gmail.com>
2024-07-24 20:16:42 +0530

    Refactore : Not that much big change

--- models/userList.js
+++ models/userList.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 //UserList for table
 const userListSchema = new mongoose.Schema({
+  // key:{type:'UUID'},
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phoneNumber: { type: String, required: true, unique: true },
+  phoneNumber: { type: Number, required: true, unique: true },
   socialHandler: { type: String, required: true },
 });
 
